319663c6798274db42193144fc3e805d8f8f840b
EmielZuurbier <emielzuurbier@outlook.com>
2016-02-05 12:36:05 +0100

    Markup edit

--- opdracht 5/static/script/script.js
+++ opdracht 5/static/script/script.js
@@ -9,31 +9,31 @@
         init: function () {
             routes.init();
         }
-    },
+    };
         
-        routes = {
-            init: function () {
-                window.location.hash = "1";
-                window.addEventListener('hashchange', function (event) {
-                    var route = window.location.hash;
-                    sections.toggle(route);
-                });
-            }
-        },
+    var routes = {
+        init: function () {
+            window.location.hash = "1";
+            window.addEventListener('hashchange', function (event) {
+                var route = window.location.hash;
+                sections.toggle(route);
+            });
+        }
+    };
     
-        sections = {
-            toggle: function (route) {
-                var section = document.querySelectorAll('section'),
-                    links = document.querySelectorAll('nav ul li a'),
-                    routeSplit = route.split('#');
-                                
-                Array.prototype.forEach.call(section, function (section) {
-                    section.classList.remove('active');
-                });
-                
-                document.getElementById(routeSplit[1]).classList.add('active');
-            }
-        };
+    var sections = {
+        toggle: function (route) {
+            var section = document.querySelectorAll('section'),
+                links = document.querySelectorAll('nav ul li a'),
+                routeSplit = route.split('#');
+
+            Array.prototype.forEach.call(section, function (section) {
+                section.classList.remove('active');
+            });
+
+            document.getElementById(routeSplit[1]).classList.add('active');
+        }
+    };
     
     app.init();
     
